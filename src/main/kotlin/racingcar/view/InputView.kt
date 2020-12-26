e852c13748235af7e54953319596a9a02b4bfb5e
lxxjn0 <ljy3510@gmail.com>
2020-12-26 23:12:46 +0900

    feat(view): 시도할 횟수를 입력받는 기능 구현

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -4,3 +4,8 @@ fun inputRacingCars(): String? {
     println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
     return readLine()
 }
+
+fun inputAttempt(): String? {
+    println("시도할 횟수는 몇 회인가요?")
+    return readLine()
+}
