7a82d526f7d08380716b8e01858470d9dce8b811
lxxjn0 <ljy3510@gmail.com>
2020-12-27 00:48:42 +0900

    refactor(view): 자동차 이름을 입력받는 기능의 로직 수정
    - 쉼표에 따라 자동차의 이름을 구분하여 문자열 리스트로 만드는 로직 추가

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,10 +1,11 @@
 package racingcar.view
 
-const val CAR_NAME_DELIMITER = ","
+const val CARS_NAME_DELIMITER = ","
 
-fun inputCarNames(): String? {
+fun inputCarNames(): List<String> {
     println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-    return readLine()
+    return readLine()!!.split(CARS_NAME_DELIMITER)
+        .map(String::trim)
 }
 
 fun inputAttempt(): String? {
