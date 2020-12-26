7d433cabb95dc7c8a6c132b3224685b713c5fb9e
lxxjn0 <ljy3510@gmail.com>
2020-12-27 01:29:37 +0900

    feat(name): NameTest 작성

---
+++ src/test/kotlin/racingcar/car/domain/NameTest.kt
@@ -0,0 +1,32 @@
+package racingcar.car.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+@Suppress("NonAsciiCharacters")
+internal class NameTest {
+    @Test
+    internal fun `Name 인스턴스 생성`() {
+        // given
+        val name = "hello"
+
+        // when
+        val actual = Name(name)
+
+        // then
+        assertThat(actual).satisfies {
+            assertThat(it).isNotNull
+            assertThat(it.name).isEqualTo(name)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["helloCar", ""])
+    internal fun `유효하지 않은 이름일 경우 예외 발생`(name: String) {
+        // then
+        assertThatIllegalArgumentException().isThrownBy { Name(name) }
+    }
+}
