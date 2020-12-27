2434511d1991bbf20760d470f5c85dfcf3718f68
lxxjn0 <ljy3510@gmail.com>
2020-12-27 15:34:30 +0900

    feat(view): 자동차가 이동한 거리를 출력하는 기능 구현

---
+++ src/main/kotlin/racingcar/ui/OutputView.kt
@@ -0,0 +1,20 @@
+package racingcar.ui
+
+import racingcar.domain.Report
+
+private const val POSITION_DELIMITER = "-"
+
+fun printReports(reports: Map<Long, List<Report>>) {
+    println("실행 결과")
+
+    for (report in reports) {
+        for ((name, position) in report.value) {
+            val stringBuilder = StringBuilder()
+            repeat(position.toInt()) {
+                stringBuilder.append(POSITION_DELIMITER)
+            }
+            println("$name: $stringBuilder")
+        }
+        println()
+    }
+}
