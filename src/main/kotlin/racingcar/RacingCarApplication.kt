83cb5cab2b648ee366ab9a301dcbd9ec9686b1b4
hungryjay <juwonson2068@naver.com>
2021-01-13 18:02:37 +0900

    [hotfix] lint 문제 해결(ktlint는 wildcard import를 허용 안함)

--- src/main/kotlin/racingcar/RacingCarApplication.kt
+++ src/main/kotlin/racingcar/RacingCarApplication.kt
@@ -3,11 +3,14 @@ package racingcar
 import racingcar.domain.Car
 import racingcar.domain.Cars
 import racingcar.domain.RacingGame
-import racingcar.view.*
+import racingcar.view.getCarNames
+import racingcar.view.getTrialNumber
+import racingcar.view.showFinalResult
+import racingcar.view.showResult
 
 fun main() {
     val carNames = getCarNames().map { name -> Car(name) }
-            .toList()
+        .toList()
     val cars = Cars(carNames)
     val trial = getTrialNumber()
 
