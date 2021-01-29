80de5b52a8e23c1b7efaa7e53990dc7cbb03ec08
hungryjay <juwonson2068@naver.com>
2021-01-29 11:23:38 +0900

    [refactor] error message 변수화

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -2,7 +2,7 @@ package racingcar.domain
 
 class Car(val name: String, var position: Position = Position()) {
     init {
-        require(name.length <= NAME_MAX_LENGTH) { "자동차 이름은 5자 이하만 가능합니다." }
+        require(name.length <= NAME_MAX_LENGTH) { NAME_LENGTH_ERROR_MESSAGE }
     }
 
     fun move(number: Int) {
@@ -13,5 +13,6 @@ class Car(val name: String, var position: Position = Position()) {
     companion object {
         const val MOVABLE_LOWER_BOUND = 4
         const val NAME_MAX_LENGTH = 5
+        const val NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 5자 이하만 가능합니다."
     }
 }
